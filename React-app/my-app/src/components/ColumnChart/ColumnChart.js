5620b97f6be9d8e46fb4db9fd6052db3e118f99c
Phoebe <proyer@revealdata.com>
2022-08-18 09:48:06 -0500

    Adding base graph component

---
+++ React-app/my-app/src/components/ColumnChart/ColumnChart.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ddc532e6d2d1d89c7be26cc867ecf58f779f54492bcd5b839cd07ffbbf44db56
+size 2116
--- React-app/my-app/src/components/FetchData/DataGridView.js
+++ React-app/my-app/src/components/FetchData/DataGridView.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ca3171702e3346dff557c7df1ab9085dfcceed6295cf17bc3b54dc0d5312dee
-size 7791
+oid sha256:2d08a36553c517abdc0731a33e764bbb8dccbb3423da29606939567bca1cb7f6
+size 7758
